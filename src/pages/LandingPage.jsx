3b772cee15fa9dd28bf629e7945cec97163d033a
unknown <mukulsinghfreelance-coder>
2026-05-27 07:15:06 +0530

    Billing

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { lazy, Suspense } from 'react'
 import { useAuth } from '../context/AuthContext'
 const PrivacyPolicy  = lazy(() => import('./PrivacyPolicy'))
